c299cb69a67fe5fe9b645854cebd89522d31b85e
Alex Dzul <alexexc2@gmail.com>
2017-09-21 18:19:14 -0500

    Agregamos soporte tenant para los media files.

--- djflow/apps/security/models.py
+++ djflow/apps/security/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.utils import timezone
+from tenant_schemas.utils import connection
 
 
 class UserProfile(models.Model):
@@ -10,7 +11,7 @@ class UserProfile(models.Model):
     def image_path(self, filename):
         extension = os.path.splitext(filename)[1][1:]
         file_name = os.path.splitext(filename)[0]
-        url = "Users/%s/profile/%s.%s" % (self.user.id, slugify(str(file_name)), extension)
+        url = "%s/Users/%s/profile/%s.%s" % (connection.tenant.domain_url, self.user.id, slugify(str(file_name)), extension)
         return url
 
     user = models.OneToOneField(User)
@@ -25,3 +26,4 @@ class UserProfile(models.Model):
 # ============================================================
 
 from .signals import *
+
